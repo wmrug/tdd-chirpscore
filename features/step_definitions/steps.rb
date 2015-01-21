69f9c152a807d1a79df4261f97c9eb63bbb0206c
Omar Qureshi <omar@omarqureshi.net>
2015-01-21 20:09:53 +0000

    added correct value

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -5,5 +5,5 @@ When(/^I specify a valid twitter handle$/) do
 end
 
 Then(/^I will see the correct value$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(page).to have_content('2.27')
 end
