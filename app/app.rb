f99918c1416ca32e17618eb875071275eef972e5
Jon Cairns <jon@ggapps.co.uk>
2015-01-21 20:27:12 +0000

    Do some stuff with twitter and sentimental

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,11 @@
 $stdout.sync = true
 require "dotenv"
+require "sentimental"
+require "twitter"
+
 ENV["RACK_ENV"] ||= "development"
 APP_ENV = ENV.fetch("RACK_ENV")
+Sentimental.load_defaults
 Dotenv.load(
   File.expand_path("../../.#{APP_ENV}.env", __FILE__),
   File.expand_path("../../.env",  __FILE__))
@@ -18,7 +22,20 @@ EOS
   end
 
   post '/result' do
-    '2.27'
+    tweets = client.user_timeline(params[:handle])[0..4]
+
+    analyzer = Sentimental.new
+
+    tweets.inject(0) { |a, e| a + analyzer.get_score(e.text) } * 2
+  end
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+    end
   end
 
   # start the server if ruby file executed directly
