2b43e565e3741a3f2a0729ec8803edef2db2f8d3
jsrn <james.srn@gmail.com>
2015-01-21 19:45:42 +0000

    added step definitions

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,7 @@
+When(/^I specify a valid twitter handle$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I will see the correct value$/) do
+  pending # express the regexp above with the code you wish you had
+end
