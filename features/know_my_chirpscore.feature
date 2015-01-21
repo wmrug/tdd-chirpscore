aec8e064e7ddf7926566a2c8e81323ca69cc7ced
Mark Shercliff <me@markshercliff.com>
2015-01-21 20:46:41 +0000

    made features betterer

--- features/know_my_chirpscore.feature
+++ features/know_my_chirpscore.feature
@@ -1,5 +1,5 @@
 Feature: I want to know what my chirpscore is
 
   Scenario: Happy Path!
-    When I specify a valid twitter handle
-    Then I will see the correct value
+    When I specify "@limeblast"
+    Then I will see 2.29
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,9 +1,9 @@
-When(/^I specify a valid twitter handle$/) do
+When(/^I specify "(.*?)"$/) do |handle|  
   visit '/'
-  fill_in 'handle', with: 'limeblast'
+  fill_in 'handle', with: handle
   click_button 'go go go!!!!'
 end
 
-Then(/^I will see the correct value$/) do
-  expect(page).to have_content('2.29')
+Then(/^I will see (\d+\.\d+)$/) do |expected_chirpscore|
+  expect(page).to have_content(expected_chirpscore)
 end
