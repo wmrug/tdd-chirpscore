140a21e9801898375d5c3e77a76026ef4f5595bb
Daniel Hollands <daniel@limeblast.co.uk>
2015-01-21 19:53:38 +0000

    go go go!!!

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,5 +1,7 @@
 When(/^I specify a valid twitter handle$/) do
-  pending # express the regexp above with the code you wish you had
+  visit '/'
+  fill_in 'handle', with: 'limeblast'
+  click_button 'go go go!!!!'
 end
 
 Then(/^I will see the correct value$/) do
