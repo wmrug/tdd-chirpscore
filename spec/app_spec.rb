66ed490cc72e73f78af22079f332e526884a3bd9
Grant Fellows <grantafellows@gmail.com>
2015-02-18 20:53:50 +0000

    Mocked twitter and Chirpscore calculator

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -6,8 +6,17 @@ RSpec.describe 'The Chirpscore App' do
     Chirpscore
   end
 
+  let(:dummy_timeline) { double("dummy_timeline") }
+
   it "renders the homepage" do
     get '/'
     expect(last_response).to be_ok
+  end
+
+  it "renders the chirpscore" do
+    allow_any_instance_of(Twitter::REST::Client).to receive(:user_timeline).with("faketwitteruser") { dummy_timeline }
+    allow_any_instance_of(ChirpscoreCalculator).to receive(:calculate).with(dummy_timeline)
+    post '/result', handle: "faketwitteruser"
+    expect(last_response.body).to have_text "123456789"
   end
 end
